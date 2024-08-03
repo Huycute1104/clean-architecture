84390cf57c4a5478aed51cbf63b2a72718a9e00a
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-08-03 19:54:57 +0700

    base

---
+++ Infrastructure/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    internal class MyDbContext : DbContext 
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options): base(options) 
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection InfrastructureService(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<MyDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Default"),
+                b => b.MigrationsAssembly(typeof(ServiceContainer).Assembly.FullName)),
+                ServiceLifetime.Scoped);
+            return services;
+        }
+    }
+}
